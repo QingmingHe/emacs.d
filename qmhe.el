dffdf39799a3bfaa9aa332d928912a4e20f1acb8
Qingming He <he_qing_ming@foxmail.com>
2014-04-04 20:37:22 +0800

    .python-environment remove, which are installed by command:: jedi:install-server

--- qmhe.el
+++ qmhe.el
@@ -63,6 +63,7 @@
 (add-hook 'python-mode-hook 'linum-mode)
 ;; use jedi for completion
 (add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
 
 ;; emacs lisp
