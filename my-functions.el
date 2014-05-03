03d6c5a649c16cab6729839f893b83fb2b73ad8d
Qingming He <906459647@qq.com>
2014-05-03 23:00:25 +0800

    * add no word but not used for it does not support UTF-8 * write my own no-word

--- my-functions.el
+++ my-functions.el
@@ -1,4 +1,5 @@
-(defun string-list-index (item alist)
-  "return index of item in a string list in a regular way"
-  (dolist (word alist)
-    ))
+(defun my-no-word (fname)
+  "Open MS word using antiword."
+  (interactive (list (read-file-name "Open MS word file: ")))
+  (shell-command (format "antiword -m UTF-8 %s > %s" fname (concat fname ".noword")))
+  (find-file (concat fname ".noword")))
