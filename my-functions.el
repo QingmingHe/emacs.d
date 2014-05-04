f10265b4d74c09a7c5891636cf20bd969b9ce31f
Qingming He <906459647@qq.com>
2014-05-04 23:05:01 +0800

    my sr-speedbar-toggle

--- my-functions.el
+++ my-functions.el
@@ -1,5 +1,23 @@
 (defun my-no-word (fname)
   "Open MS word using antiword."
   (interactive (list (read-file-name "Open MS word file: ")))
-  (shell-command (format "antiword -m UTF-8 %s > %s" fname (concat fname ".noword")))
-  (find-file (concat fname ".noword")))
+  (let (
+        (tmp-file-name (format (concat "/tmp/" "." (file-name-nondirectory fname))))
+        )
+    (progn
+      (shell-command
+       (format "antiword -m UTF-8 %s > %s" fname tmp-file-name))
+      (find-file tmp-file-name)
+      )
+    )
+  )
+
+(defun my-shell-command-to-string (command)
+  "Return the STDOUT of shell command without the line break."
+  (substring (shell-command-to-string command) 0 -1))
+
+(defun os-path-basename (path)
+  "Return the basename of path. Just like os.path.basename in Python"
+  (car (last (split-string path "/"))))
+
+
