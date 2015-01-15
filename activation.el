5cfab5a6b91b22eac17b4fa73fd5506c499081e0
Qingming He <906459647@qq.com>
2015-01-15 19:27:36 +0800

    change load sequence

--- activation.el
+++ activation.el
@@ -1,5 +1,9 @@
-(starter-kit-load "eshell")
+;; desktop should be the first
 (starter-kit-load "desktop")
+;; Configure org 8.* bundled with Emacs 24.4
+(when (> emacs-minor-version 3)
+  (starter-kit-load "org"))
+(starter-kit-load "eshell")
 (starter-kit-load "ibuffer")
 (starter-kit-load "ctags")
 (starter-kit-load "flycheck")
@@ -9,21 +13,18 @@
 (starter-kit-load "evil")
 (starter-kit-load "yasnippet")
 (starter-kit-load "autocomplete")
-;; the author don't use company
+(starter-kit-load "w3m")
+(starter-kit-load "dictionary")
 (starter-kit-load "company")
 (starter-kit-load "bindings")
+(starter-kit-load "ansiterm")
+;; prog mode should be loaded finally
 (starter-kit-load "python")
 (starter-kit-load "nxml")
 (starter-kit-load "lisp")
 (starter-kit-load "cmake")
 (starter-kit-load "rst")
-(starter-kit-load "w3m")
-(starter-kit-load "ansiterm")
 (starter-kit-load "fortran")
-(starter-kit-load "dictionary")
-;; Configure org 8.* bundled with Emacs 24.4
-(when (> emacs-minor-version 3)
-  (starter-kit-load "org"))
 (starter-kit-load "latex")
 (starter-kit-load "graphviz")
 (starter-kit-load "matlab")
