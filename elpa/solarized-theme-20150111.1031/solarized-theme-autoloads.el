4a5453cf86dd47b55595a2515394cb01692c42a4
Qingming He <906459647@qq.com>
2015-01-26 11:15:22 +0800

    solarized theme real works now

--- elpa/solarized-theme-20150111.1031/solarized-theme-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; solarized-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "solarized" "solarized.el" (21691 51423 434959
-;;;;;;  100000))
-;;; Generated autoloads from solarized.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("solarized-dark-theme.el" "solarized-light-theme.el"
-;;;;;;  "solarized-theme-pkg.el" "solarized-theme-utils.el" "solarized-theme.el")
-;;;;;;  (21691 51424 179001 700000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; solarized-theme-autoloads.el ends here
