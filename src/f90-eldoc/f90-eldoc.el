98837a4152c6c677dc5661a6a8b95cad0d086737
Qingming He <906459647@qq.com>
2015-02-07 16:40:31 +0800

    f90-eldoc can recognize key word argument

--- src/f90-eldoc/f90-eldoc.el
+++ src/f90-eldoc/f90-eldoc.el
@@ -56,7 +56,7 @@
       (setq yes? t))
     yes?))
 
-(defun f90-eldoc-update (sym arg-index)
+(defun f90-eldoc-update (sym kw-or-i)
   (when sym
     (let (doc sym-plist)
       (setq sym-plist (f90-eldoc-get-sym-plist sym))
@@ -76,15 +76,15 @@
                 (line-beginning-position) (line-end-position))))
           (setq doc (plist-get sym-plist :doc)))
         (setq sym-plist (plist-put sym-plist :doc doc))
-        (setq sym-plist (f90-eldoc-font-lock sym-plist arg-index))
+        (setq sym-plist (f90-eldoc-font-lock sym-plist kw-or-i))
         (puthash sym sym-plist f90-eldoc-cache))
       sym-plist)))
 
-(defun f90-eldoc-font-lock (sym-plist arg-index)
+(defun f90-eldoc-font-lock (sym-plist kw-or-i)
   (when sym-plist
     (let (last-pos
           current-arg-index
-          (arg-index (or arg-index -1)))
+          current-kword)
       (with-temp-buffer
         (erase-buffer)
         (goto-char (point-min))
@@ -99,46 +99,46 @@
                              'face font-lock-function-name-face))
         (setq last-pos (point))
         (setq current-arg-index 0)
-        (while (re-search-forward "[,)]" nil t)
+        (while (re-search-forward "\\([a-zA-Z0-9_]+\\)[ \t]*[,)]" nil t)
+          (setq current-kword (match-string 1))
           (setq current-arg-index (1+ current-arg-index))
-          (if (= current-arg-index arg-index)
-              (put-text-property last-pos (1- (point)) 'face 'bold)
-            (put-text-property last-pos (1- (point)) 'face
-                               font-lock-variable-name-face))
+          (cond ((and
+                  (stringp kw-or-i)
+                  (string= kw-or-i (downcase current-kword)))
+                 (put-text-property last-pos (1- (point)) 'face 'bold))
+                ((and
+                  (number-or-marker-p kw-or-i)
+                  (= current-arg-index kw-or-i))
+                 (put-text-property last-pos (1- (point)) 'face 'bold))
+                (t
+                 (put-text-property last-pos (1- (point)) 'face
+                                    font-lock-variable-name-face)))
           (setq last-pos (point)))
         (setq sym-plist (plist-put sym-plist :doc (buffer-string))))
       sym-plist)))
 
-(defun f90-eldoc-args-index-or-kw (beg end)
+(defun f90-eldoc-args-index-or-kw (beg)
   (let (lbnd rbnd index kword)
     (save-excursion
       (setq lbnd (re-search-backward "[,(]" beg t)))
     (save-excursion
-      (setq rbnd (re-search-forward "[,)]" end t)))
+      (setq rbnd (re-search-forward "[,)]" nil t)))
     (when (and lbnd rbnd)
       (save-excursion
         (goto-char lbnd)
-        (when (re-search-forward "[ \t]*\\([a-zA-Z0-9_]+\\).*=" rbnd nil)
+        (when (re-search-forward "[ \t]*\\([a-zA-Z0-9_]+\\)[& \t\n]*=" rbnd t)
           (setq kword (downcase (match-string 1))))))
     (unless kword
-      (setq index 0)
+      (setq index 1)
       (save-excursion
-        (while (re-search-backward "\\((.*)\\)?[ \t]*[,(]" beg t)
+        (while (re-search-backward "[a-zA-Z0-9_]+[ \t]*\\((.*)\\)?[ \t]*," beg t)
           (setq index (1+ index)))))
     (or kword index)))
-
-(defun f90-eldoc-args-index (beg)
-  (when beg
-    (let ((index 0))
-      (save-excursion
-        (while (re-search-backward "\\((.*)\\)?[ \t]*[,(]" beg t)
-          (setq index (1+ index))))
-      index)))
 
 (defun f90-eldoc-function ()
   (let (sym
         sym-plist
-        args-index
+        kw-or-i
         (sp (syntax-ppss)))
     (unless (nth 4 sp)
       (save-excursion
@@ -147,13 +147,13 @@
         (unless (looking-back "\\(function\\|subroutine\\).*")
           (setq sym (symbol-at-point))))
       (when (and sym (not (eq 0 (nth 0 sp))))
-        (setq args-index (f90-eldoc-args-index (nth 1 sp))))
+        (setq kw-or-i (f90-eldoc-args-index-or-kw (nth 1 sp))))
       (if (f90-eldoc-should-update sym)
-          (setq sym-plist (f90-eldoc-update sym args-index))
+          (setq sym-plist (f90-eldoc-update sym kw-or-i))
         (setq sym-plist
               (f90-eldoc-font-lock
                (gethash sym f90-eldoc-cache)
-               args-index))))
+               kw-or-i))))
     (plist-get sym-plist :doc)))
 
 (provide 'f90-eldoc)
