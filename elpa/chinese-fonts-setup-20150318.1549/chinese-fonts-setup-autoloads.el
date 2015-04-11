9e2b5f62b52d5dfc52329f090c71bbe9b53d05c2
Qingming He <906459647@qq.com>
2015-04-11 20:06:55 +0800

    add chinese fonts setup

---
+++ elpa/chinese-fonts-setup-20150318.1549/chinese-fonts-setup-autoloads.el
@@ -0,0 +1,16 @@
+;;; chinese-fonts-setup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("chinese-fonts-setup.el") (21800 56646
+;;;;;;  413278 700000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; chinese-fonts-setup-autoloads.el ends here
