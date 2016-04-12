0cd9477f73b90d7866438b305bbcb26d3886c174
Qingming He <906459647@qq.com>
2016-04-12 21:55:57 +0800

    some modifications of necp_x

--- project-locals/necp_x.el
+++ project-locals/necp_x.el
@@ -7,6 +7,18 @@
               (add-to-list 'flycheck-fortran+-include-paths
                            (expand-file-name "src/utilities/"))
             (add-to-list 'flycheck-fortran+-definitions "DBL_REAL")
+            (add-to-list
+             'flycheck-fortran+-definitions
+             (format
+              "necl_ce_default=\"'%s'\""
+              (expand-file-name
+               "../build_syntax/library/necl_ce_endf70")))
+            (add-to-list
+             'flycheck-fortran+-definitions
+             (format
+              "necl_mg_default=\"'%s'\""
+              (expand-file-name
+               "../build_syntax/library/necl_gm_endf70_wims69e")))
             (setq flycheck-fortran+-module-path
                   (expand-file-name "../build_syntax/src")))
           (unless flycheck-mode
