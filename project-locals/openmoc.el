a9d0485f6fb1e00da4b69fd2a5fcc5da032e624b
Qingming He <906459647@qq.com>
2016-10-10 20:30:51 -0400

    update

--- project-locals/openmoc.el
+++ project-locals/openmoc.el
@@ -3,12 +3,15 @@
       . (progn
           (when (string-match-p "\\.h$" buffer-file-name)
             (call-interactively 'c++-mode))))))
+ (nil
+  . ((eval
+      . (progn
+          (setq-local compile-command "python setup.py install --user --fp=double")
+          (setq-local compile-dir-default (cdr project-details))))))
  (c++-mode
   . ((eval
       . (progn
           (setq-local flycheck-gcc-openmp t)
-          (setq-local compile-command "python setup.py install --user --fp=double")
-          (setq-local compile-dir-default (cdr project-details))
           (prj/set-language-flags
            `(,(getenv "_PYTHON_INC_PATH")
              ,@(prj/c++-system-include-paths))
