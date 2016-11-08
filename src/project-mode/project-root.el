76174825cee1b3bf67ba6272ae6e113179fc5faf
Qingming He <906459647@qq.com>
2016-11-08 12:10:47 -0500

    add i

--- src/project-mode/project-root.el
+++ src/project-mode/project-root.el
@@ -150,7 +150,7 @@ Use this to exclude portions of your project: \"-not -regex \\\".*vendor.*\\\"\"
   (let ((maps
          '(org md rst txt c cc cxx cpp c++ h hpp hxx java f F for FOR f77 F77
                f90 F90 f95 F95 f03 F03 f08 F08 inc py pl rb el lisp cl scheme
-               mk)))
+               mk i)))
     (if (executable-find "ctags")
         (regexify-ext-list
          (with-temp-buffer
