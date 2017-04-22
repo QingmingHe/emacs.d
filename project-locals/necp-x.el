b1767eff5896da8d89095d6cae30fb116238cf9b
Qingming He <906459647@qq.com>
2017-04-22 11:18:24 -0400

    compile-dir-defualt

--- project-locals/necp-x.el
+++ project-locals/necp-x.el
@@ -4,7 +4,6 @@
           (setq whilecpace-cleanup-mode-only-if-initially-clean nil)
           (when (derived-mode-p 'fortran-mode 'f90-mode)
             (with-project-root
-                (setq-local compile-dir-default (expand-file-name ".."))
                 (add-to-list 'flycheck-fortran+-include-paths
                              (expand-file-name "src/utilities/"))
               (add-to-list 'flycheck-fortran+-definitions "DBL_REAL")
@@ -20,6 +19,14 @@
                 "necl_mg_default=\"'%s'\""
                 (expand-file-name
                  "library/necl_gm_endf70_wims69e")))
+              (if (getenv "NECP_X_build_syntax")
+                  (setq-local
+                   compile-dir-default
+                   (expand-file-name
+                    (concat
+                     (file-name-as-directory
+                      (getenv "NECP_X_build_syntax")) "..")))
+                (setq-local compile-dir-default (expand-file-name "..")))
               (setq flycheck-fortran+-module-path
                     (if (getenv "NECP_X_build_syntax")
                         (concat (file-name-as-directory
