dcff4f3f725023a6e1f4ba0ed25199405966515a
Qingming He <he_qing_ming@foxmail.com>
2017-10-09 14:05:21 +0800

    add helm bibtex

---
+++ elpa/biblio-core-20160901.1115/biblio-core-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "biblio-core" "20160901.1115" "A framework for looking up and displaying bibliographic entries" '((emacs "24.3") (let-alist "1.0.4") (seq "1.11") (dash "2.12.1")) :authors '(("Cl\303\251ment Pit-Claudel" . "clement.pitclaudel@live.com")) :maintainer '("Cl\303\251ment Pit-Claudel" . "clement.pitclaudel@live.com") :url "http://github.com/cpitclaudel/biblio.el")
---
+++ elpa/parsebib-2.3.1/parsebib-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "parsebib" "2.3.1" "A library for parsing bib files" '((emacs "24.3")) :commit "bc31b627c666df576aa37e21c27a2223b3cb91a3" :keywords '("text" "bibtex"))
