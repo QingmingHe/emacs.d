5862ca5ca43767e5423ef64d12e5e550e8011bf4
Qingming He <he_qing_ming@foxmail.com>
2017-12-08 15:01:54 +0800

    remove irony and chinese

--- elpa/chinese-fonts-setup-20150318.1549/chinese-fonts-setup-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "chinese-fonts-setup" "20150318.1549" "A fonts config tool enforcing double-width Chinese character display" '((cl-lib "0.5")) :url "https://github.com/tumashu/chinese-fonts-setup" :keywords '("convenience" "chinese" "font"))
